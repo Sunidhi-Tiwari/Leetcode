8ff5be80ca2fecbdf0b5fb7a639c22897b8250de
Sunidhi-Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-05-01 09:44:44 +0530

    Time: 20 ms (9.18%), Space: 13.5 MB (96.52%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,16 +4,20 @@ public:
         
         int n = cost.size();
         
-        int dp[n+1];
         
-        dp[1] = 0;
-        dp[0] = 0;
         
+        int prev1 = 0;
+        int prev2 = 0;
+        
+        int curi;
         for(int i = 2; i<=n; i++)
         {
-            dp[i] = min({dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]});
+            curi = min({prev1 + cost[i-1], prev2 + cost[i-2]});
+            
+            prev2 = prev1;
+            prev1 = curi;
         }
         
-        return dp[n];
+        return curi;
     }
 };
