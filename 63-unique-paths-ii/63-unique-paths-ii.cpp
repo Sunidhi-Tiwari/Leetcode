d26f8f37d146c3690884208cff9d5e882b38157c
Sunidhi-Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-05-06 16:50:34 +0530

    Time: 5 ms (40.46%), Space: 7.9 MB (41.04%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -9,43 +9,44 @@ public:
            return 0;
            
        vector<vector<int>> dp(m, vector<int>(n,-1));
+       dp[0][0] = 1;
+        
+       for(int i = 0; i<m; i++)
+       {
+           for(int j = 0; j<n; j++)
+           {   
+               
+               if(i == 0 && j == 0)
+               {
+                   dp[i][j] = 1;
+               }
+               else
+               {
+                   int down = 0;
+                   int right = 0;
+               if(i>0) 
+               {
+                   if(obstacleGrid[i-1][j] != 1)
+                   down = dp[i-1][j];
+               }
+               
+               if(j>0)
+               {
+                   if(obstacleGrid[i][j-1] != 1)
+                   right = dp[i][j-1];
+               }
+                    dp[i][j] = down + right;
+               }
+               
+              
+           }
+       }
         
         
         
-       return f(m-1,n-1,obstacleGrid, dp);
+       return dp[m-1][n-1];
         
     }
     
-    int f(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp)
-    {
-        if(i == 0 && j == 0)
-           return dp[i][j] = 1;
-        if(i<0 || j<0) return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int up = 0;
-        int left = 0;
-        
-        if(i>0)
-        {
-             if(obstacleGrid[i-1][j] != 1)
-             {
-                 up = f(i-1,j,obstacleGrid,dp);
-             }
-       
-       
-        }
-            
-        if(j>0)
-        {
-              if(obstacleGrid[i][j-1] != 1)
-              {
-                  left = f(i,j-1,obstacleGrid,dp);
-              }
-        }
-        
-        return dp[i][j] = up + left;
-    }
+    
 };
