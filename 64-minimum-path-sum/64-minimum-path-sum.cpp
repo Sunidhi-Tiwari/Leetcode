aab0fc5ee5b63183d493240a1ce0e0793916dc5c
Sunidhi-Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-05-06 17:55:49 +0530

    Time: 19 ms (18.43%), Space: 10.2 MB (6.64%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -7,35 +7,16 @@ public:
         
         vector<vector<int>> dp(m, vector<int> (n,-1));
         
-        
-        
-        for(int i = 0; i<m; i++)
-        {
-            for(int j = 0; j<n; j++)
-            {
-                if (i == 0 && j == 0)
-                dp[i][j] = grid[i][j];
-                else
-                {    
-                     int up = INT_MAX;
-                     int left = INT_MAX; 
-                     if(i>0) up = grid[i][j] + dp[i-1][j];
-                     if(j>0) left = grid[i][j] + dp[i][j-1];
-                     dp[i][j] = min(up,left);
-                }
-            }
-        }
-        
-        return dp[m-1][n-1];
+        return f(m-1,n-1,grid, dp);
         
     }
     
-   /* int f(int i, int j, vector<vector<int>> grid, vector<vector<int>> dp )
+    int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp )
     {
         if (i == 0 && j == 0)
-            return dp[i][j] = grid[i][j];
+            return grid[i][j];
         if(i<0 || j<0)
-            return 0;
+            return INT_MAX;
         
         if(dp[i][j] != -1)
             return dp[i][j];
@@ -46,6 +27,5 @@ public:
         if(j>0) left = grid[i][j] + f(i,j-1,grid, dp);
         
         return dp[i][j] = min(up,left);
-    }*/
-    
+    }
 };
