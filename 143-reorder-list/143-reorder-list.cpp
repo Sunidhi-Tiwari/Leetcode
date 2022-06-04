bbfcf73df43351df55045a3f81dae5c36016865b
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-04 09:49:31 +0530

    Time: 44 ms (83.53%), Space: 17.8 MB (77.68%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,22 +10,36 @@
  */
 class Solution {
 public:
+     ListNode* middleNode(ListNode* head) {
+        auto slow = head, fast = head;
+        while(fast && fast -> next)
+            slow = slow -> next,
+            fast = fast -> next -> next;            // fast moves at 2x speed
+        return slow;                                // slow ends up at mid
+    }
+	// 206. Reverse Linked List - reverses and returns the head of reversed list
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        while(head) {
+            auto nextNode = head -> next;           // store next node before reversing next ptr of cur
+            head -> next = prev;                    // reverse the next ptr to previous node
+            prev = head;                            // update previous node as cur
+            head = nextNode;                        // move to orignal next node
+        }
+        return prev;                                // returns head of reversed list
+    }
     void reorderList(ListNode* head) {
-        
-       vector<ListNode*> arr;
-        for(auto iter = head; iter; iter = iter -> next)
-            arr.push_back(iter);
-        
-		// pointers to start and end of list. Re-order in alternating fashion from both end
-        int L = 1, R = size(arr)-1;
-        for(int i = 0; i < size(arr); i++, head = head -> next) 
-            if(i & 1)                             // odd iteration:
-                head -> next = arr[L++];          //        - pick node from L & update L ptr
-            else                                  // even iteration
-                head -> next = arr[R--];          //        - pick node from R & update R ptr
-        
-        head -> next = nullptr;
-        
-        
+        if(!head || !head -> next) return;
+        auto mid = middleNode(head);
+        auto R = reverseList(mid), L = head -> next;
+        for(int i = 0; L != R; i++, head = head -> next)     // re-order in alternating fashion 
+            if(i & 1) {                              
+                head -> next = L;
+                L = L -> next;
+            }
+            else {
+                head -> next = R;
+                R = R -> next;
+            }
     }
 };
