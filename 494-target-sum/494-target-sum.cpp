9cdaa1fc215d7cd043fc1d232bbdfc3cc7aa9e9e
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-04 22:26:47 +0530

    Time: 25 ms (57.52%), Space: 12 MB (45.36%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -11,33 +11,50 @@ public:
         
         int tar = (sum-target)/2;
         
-        vector<vector<int>> dp(n, vector<int>(tar+1,-1));
+        vector<vector<int>> dp(n, vector<int>(tar+1,0));
         
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
         
-        int cnt = f(n-1, tar, nums, dp);
+        if(nums[0] != 0 && nums[0]<=tar) dp[0][nums[0]] = 1;
         
-        return cnt;
+        for(int i = 1; i<n; i++)
+        {
+            for(int t = 0; t<=tar; t++)
+            {
+                int nottake = dp[i-1][t];
+                int take = 0;
+                if(nums[i] <= t) take = dp[i-1][t-nums[i]];
+                
+                dp[i][t] = nottake + take;
+            }
+        }
+        
+    
+        
+        return dp[n-1][tar];
         
         
     }
     
-    int f(int ind, int tar, vector<int>& nums,  vector<vector<int>> &dp)
-    {
-        if(ind == 0)
-        {
-            if(nums[0] == 0 && tar == 0) return 2;
-            if(tar == 0 || nums[0] == tar) return 1;
-            return 0;
-        }
-        
-        if(dp[ind][tar] != -1) return dp[ind][tar];
-        
-        int nottake = f(ind-1, tar, nums, dp);
-            
-        int take = 0;
-        
-        if(nums[ind]<=tar) take = f(ind-1, tar-nums[ind], nums, dp);
-            
-        return dp[ind][tar] = nottake + take;
-    }
 };
+    
+//     int f(int ind, int tar, vector<int>& nums,  vector<vector<int>> &dp)
+//     {
+//         if(ind == 0)
+//         {
+//             if(nums[0] == 0 && tar == 0) return 2;
+//             if(tar == 0 || nums[0] == tar) return 1;
+//             return 0;
+//         }
+        
+//         if(dp[ind][tar] != -1) return dp[ind][tar];
+        
+//         int nottake = f(ind-1, tar, nums, dp);
+            
+//         int take = 0;
+        
+//         if(nums[ind]<=tar) take = f(ind-1, tar-nums[ind], nums, dp);
+            
+//         return dp[ind][tar] = nottake + take;
+//     }
