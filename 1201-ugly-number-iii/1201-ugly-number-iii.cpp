2b31bcdc3fad2296cf5e21225421994d07d93d06
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-05 18:01:11 +0530

    Time: 2 ms (43.26%), Space: 6 MB (30.84%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.cpp
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -1,35 +1,46 @@
 typedef long long ll;
 class Solution {
 public:
-    ll lcm(ll a, ll b) {
-        return a*b / __gcd(a, b);
-    }
     
-    int nthUglyNumber(int n, int a, int b, int c) 
-    {
-        ll N = (ll)n, A = ll(a), B = ll(b), C = ll(c);
-        ll lo = 1, hi = 2000000000, mid, ans;
-        
-        while(lo <= hi)
+     ll lcm(ll a, ll b)
         {
-            mid = lo + (hi - lo)/2;
-            ll cnt = 0;
-            cnt += mid/A;
-            cnt += mid/B;
-            cnt += mid/C;
-            cnt -= mid / lcm(A, B);
-            cnt -= mid / lcm(B, C);
-            cnt -= mid / lcm(C, A);
-            cnt += mid / lcm(a, lcm(b, c));
-            
-            if(cnt >= N)
-            {
-                ans = mid;
-                hi = mid-1;
-            }
-            else
-                lo = mid+1;
+          return (a*b)/__gcd(a,b);
         }
-        return ans;
+    int nthUglyNumber(int n, int a, int b, int c) {
+        
+      ll N = ll(n); 
+      ll A = ll(a);
+      ll B = ll(b);
+      ll C = ll(c);
+      
+       ll lo = 1;
+       ll hi = 2000000000;
+       ll ans;
+       ll mid;
+      
+      while(lo<=hi)
+      {
+      
+      mid = lo + (hi - lo)/2;
+      
+      ll cnt = 0;
+      
+      cnt += (mid/A + mid/B + mid/C);
+      cnt -= (mid/lcm(A,B) + mid/lcm(B,C) + mid/lcm(C,A));
+      cnt += mid/lcm(A, lcm(B,C));
+      
+      if(cnt>=N)
+      {
+      ans = mid;
+      hi = mid - 1;
+      }
+      else
+      {
+      lo = mid + 1;
+      }
+      
+      }
+      
+      return ans;
     }
 };
