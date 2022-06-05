9bc0367b1f6ebc1b59d21ee6a613af0e5cdc4912
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-05 18:11:09 +0530

    Time: 4 ms (37.22%), Space: 5.9 MB (22.24%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -4,10 +4,10 @@ public:
         
         if(n<=0) return false;
         
-        while(n%2 == 0) n = n/2;
+       
         while(n%3 == 0) n = n/3;
         while(n%5 == 0) n = n/5;
         
-        return n == 1;
+        return (n & (n-1)) == 0;
     }
 };
