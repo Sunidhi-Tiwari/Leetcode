645dda20793d2bc210158f09a8867da407e5777e
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-06 10:18:50 +0530

    Time: 11 ms (63.12%), Space: 10.9 MB (61.68%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    
+    void f(int ind, int target, vector<int>& candidates, vector<vector<int>> & ans, vector<int> &ds)
+    {
+        if(ind == candidates.size())
+        {
+            if(target == 0)
+                ans.push_back(ds);
+            return;
+        }
+        
+        if(candidates[ind] <= target)
+        {
+            ds.push_back(candidates[ind]);
+            f(ind, target-candidates[ind], candidates, ans, ds);
+            ds.pop_back();
+        }
+        
+         f(ind + 1, target, candidates, ans, ds);
+    }
+    
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        f(0, target, candidates, ans, ds);
+        
+        return ans;
+    }
+};
