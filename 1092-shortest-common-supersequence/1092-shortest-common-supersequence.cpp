cd30ac7a3d83428c311a62b05da9b3f674f4ba99
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-08 13:33:18 +0530

    Time: 17 ms (71.08%), Space: 10.7 MB (85.44%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,27 +1,61 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string& A, string& B) {
-        int i = 0, j = 0;
-        string res = "";
-        for (char c : lcs(A, B)) {
-            while (A[i] != c)
-                res += A[i++];
-            while (B[j] != c)
-                res += B[j++];
-            res += c, i++, j++;
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n=s1.size(); 
+        int m=s2.size();
+        int t[n+1][m+1]; //table generated while computing LCS length
+        string res; // result	
+        // compute LCS length using tabulation      
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0||j==0)
+                t[i][j]=0;
+            else if(s1[i-1]==s2[j-1])
+            {
+                t[i][j]=1+t[i-1][j-1];
+            }
+            else
+            {
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
         }
-        return res + A.substr(i) + B.substr(j);
+    }     
+       //print lcs
+    int i=n,j=m;
+    while(i>0 && j>0)
+    {
+        if(s1[i-1]==s2[j-1])
+        {
+            res.push_back(s1[i-1]);
+            i--;
+            j--;
+        }
+        else
+        {
+            if(t[i-1][j]>t[i][j-1])
+            {
+                res.push_back(s1[i-1]);
+                i--;
+            } else{
+                res.push_back(s2[j-1]);
+                j--;
+            }
+        }
     }
-
-    string lcs(string& A, string& B) {
-        int n = A.size(), m = B.size();
-        vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (A[i] == B[j])
-                    dp[i + 1][j + 1] = dp[i][j] + A[i];
-                else
-                    dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
-        return dp[n][m];
+    while(i>0) // if s1 characters are still left
+    {
+        res.push_back(s1[i-1]);
+        i--;
+    }
+    while(j>0) //if s2 characters are still left
+    {
+        res.push_back(s2[j-1]);
+        j--;
+        
+    }
+        reverse(res.begin(),res.end()); 
+        return res;
     }
 };
