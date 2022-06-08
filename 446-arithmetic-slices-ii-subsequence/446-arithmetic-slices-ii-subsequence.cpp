0115885b41460604596ca7e94fa0f36ef643b2ba
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-08 17:17:16 +0530

    Time: 1294 ms (38.37%), Space: 154.7 MB (57.27%) - LeetHub

--- 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.cpp
+++ 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-       int res = 0;
+    int numberOfArithmeticSlices(vector<int>& nums) {
         
-        unordered_map<int, vector<int>> a_idx;
-    vector<vector<int>> dp(A.size(), vector<int>(A.size()));
-    for (auto i = 0; i < A.size(); ++i)
-        a_idx[A[i]].push_back(i);
-    for (auto i = 0; i < A.size(); ++i)
-        for (int j = 0; j < i; j++) {
-            auto prev = 2l * A[j] - A[i];
-            if (prev < INT_MIN || prev > INT_MAX)
-                continue;
-            auto it = a_idx.find(prev);
-            if (it != end(a_idx)) {
-                for (auto k : it->second) {
-                    if (k >= j)
-                        break;
-                    dp[i][j] += dp[j][k] + 1;
+         vector< unordered_map<long, long> > dp(nums.size());
+        int res = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long)nums[i] - (long)nums[j];
+                
+                dp[i][diff]++;
+                
+                if (dp[j].find(diff) != dp[j].end()) {
+                    dp[i][diff] += dp[j][diff];
+                    res += dp[j][diff];
                 }
             }
-            res += dp[i][j];
         }
-    return res;
+        
+        return res;
     
         
     }
