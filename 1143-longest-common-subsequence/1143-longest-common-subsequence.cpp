a774fa3c1716bbacff921bab1b0875a1a1fe1565
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-08 09:48:20 +0530

    Time: 54 ms (15.21%), Space: 13.3 MB (10.66%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,11 +3,11 @@ public:
     
     int f(int i, int j, string &text1, string &text2, vector<vector<int>> &dp)
     {
-        if(i<0 || j<0) return 0;
+        if(i == 0 || j == 0) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(text1[i] == text2[j]) return dp[i][j] = 1 + f(i-1,j-1,text1, text2, dp);
+        if(text1[i-1] == text2[j-1]) return dp[i][j] = 1 + f(i-1,j-1,text1, text2, dp);
         
         return dp[i][j] = max(f(i-1,j,text1, text2, dp), f(i,j-1,text1, text2, dp));
     }
@@ -17,9 +17,9 @@ public:
         int n = text1.size();
         int m = text2.size();
         
-        vector<vector<int>> dp(n, vector<int>(m,-1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
         
-        return f(n-1, m-1, text1, text2, dp);
+        return f(n, m, text1, text2, dp);
         
     }
 };
