c8fcb03636ffa699c1496adfdd29382cc979df1a
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-09 13:27:51 +0530

    Time: 19 ms (54.33%), Space: 9.1 MB (37.55%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     // tabulation
-    int f(int i,  int j, string &word1, string &word2,vector<vector<int>> &dp)
-    {   
-        if(j<0) return i+1;
-        if(i<0) return j+1;
-        
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(word1[i] == word2[j]) return dp[i][j] = f(i-1,j-1, word1, word2, dp);
-        return dp[i][j] = 1 + min({f(i-1,j, word1, word2, dp) , f(i, j-1, word1, word2, dp) , f(i-1,j-1,word1, word2, dp)});
-    }
-    
+   
     int minDistance(string word1, string word2) {
         
         int n = word1.length();
