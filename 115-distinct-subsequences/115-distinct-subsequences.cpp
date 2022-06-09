739f61ee4e0b77f8dab8ffe14905c1dc316efcec
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-09 09:55:23 +0530

    Time: 62 ms (37.17%), Space: 30 MB (26.40%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -23,9 +23,22 @@ public:
         
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n, vector<int> (m,-1));
+        vector<vector<double>> dp(n+1, vector<double> (m+1, 0));
         
-        return f(n-1,m-1, s, t, dp);
+        for(int i = 0; i<=n; i++) dp[i][0] = 1;
+        
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=m; j++)
+            {
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return (int)dp[n][m];
         
     }
 };
