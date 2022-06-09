eb9568bcf4b094b7f6dd54e37857427c30f9c703
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-09 13:27:35 +0530

    Time: 21 ms (47.60%), Space: 9.1 MB (37.55%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // tabulation
     int f(int i,  int j, string &word1, string &word2,vector<vector<int>> &dp)
     {   
         if(j<0) return i+1;
@@ -18,8 +18,24 @@ public:
         int n = word1.length();
         int m = word2.length();
         
-        vector<vector<int>> dp(n, vector<int>(m,-1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
         
-        return f(n-1,m-1, word1, word2, dp);
+        for(int i = 0; i<=n; i++) dp[i][0] = i;
+        for(int j = 0; j<=m; j++) dp[0][j] = j;
+        
+        for(int i = 1; i<=n; i++)
+        {
+              for(int j = 1; j<=m; j++)
+              {
+                  if(word1[i-1] == word2[j-1])
+                      dp[i][j] = dp[i-1][j-1];
+                  else
+                      dp[i][j] = 1 + min({dp[i-1][j] , dp[i-1][j-1] , dp[i][j-1]});
+              }
+        }
+        
+        
+        
+        return dp[n][m];
     }
 };
