635f47e5deccdc075086ba5a12be130d82666f4a
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-10 15:14:20 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.56%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& grid) {
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        for(int i = 0; i<n; i++)
-        {
-            for(int j = i; j<m; j++)
-            {
-                swap(grid[i][j], grid[j][i]);
-            }
-        }
-        
-        int k = m/2;
-        
-        for(int k = 0; k<m/2; k++)
-        {
-            for(int i = 0; i<n; i++)
-                swap(grid[i][k], grid[i][m-1-k]);
-        }
-        
-        return;
+    void rotate(vector<vector<int>>& matrix) {
+     reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);  } 
+      return;
     }
 };
