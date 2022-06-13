15fe6f7e27539eb42c636922c9c6173e79f9c853
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-13 14:39:22 +0530

    Time: 342 ms (32.51%), Space: 10.8 MB (44.33%) - LeetHub

--- 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.cpp
+++ 1542-find-longest-awesome-substring/1542-find-longest-awesome-substring.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestAwesome(string s) {
         
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         
         int mask = 0;
         int ans = 0;
@@ -10,7 +10,7 @@ public:
         
         for(int i = 0; i<s.length(); i++)
         {
-            mask = (mask^(1<<(s[i]-'0')));
+            mask ^= (1<<(s[i]-'0'));
             
             if(mp.find(mask) != mp.end())
                 ans = max(ans, i-mp[mask]);
