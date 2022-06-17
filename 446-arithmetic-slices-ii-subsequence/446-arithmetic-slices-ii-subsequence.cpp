c193a9b19280e00b8777b1f84d7f27219e7b15e5
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-17 19:01:31 +0530

    Time: 933 ms (71.83%), Space: 154.8 MB (40.36%) - LeetHub

--- 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.cpp
+++ 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
+     
+        int n = nums.size();
+        vector< unordered_map<long long,int> > freq(n);
         
-         vector< unordered_map<long, long> > dp(nums.size());
-        int res = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                long diff = (long)nums[i] - (long)nums[j];
-                
-                dp[i][diff]++;
-                
-                if (dp[j].find(diff) != dp[j].end()) {
-                    dp[i][diff] += dp[j][diff];
-                    res += dp[j][diff];
-                }
+        int ans = 0;
+        for(int i = 1; i<n; i++)
+        {
+            for(int j = 0; j<i; j++)
+            {
+            long long diff = (long long)nums[j] - (long long)nums[i];
+            
+            int cnt = (freq[j].count(diff)) ? freq[j][diff] : 0;
+            freq[i][diff] += cnt + 1;
+            ans += cnt;
             }
         }
         
-        return res;
-    
+        
+     return ans;
         
     }
 };
