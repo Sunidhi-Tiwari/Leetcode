24deb3746086646b0ee314b569a608b66a8efa2a
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-18 22:58:18 +0530

    Time: 25 ms (92.56%), Space: 12.4 MB (22.85%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,74 +1,60 @@
 class Solution {
 public:
-    
-    
-          int lower_bound(vector<int>&v, int x)
-	{
-        int lo = 0, hi = v.size() - 1;
-		while(lo<=hi)
-		{
-			int mid = lo + (hi-lo)/2;
-			if(v[mid] >= x){
-				hi = mid - 1;
-			}
-			else
-				lo = mid + 1;
-		}
-		return lo;
-	}
-	
-    int minimumMountainRemovals(vector<int>& nums) 
-    {
-        int n=nums.size();
-        vector<int> inc(n), dec(n), v;
-		
-        for(int i = 0; i < nums.size(); i++)
+    int minimumMountainRemovals(vector<int>& nums) {
+       
+        int n = nums.size();
+        vector<int> inc(n);
+        vector<int> dec(n);
+        vector<int> v;
+        
+        for(int i = 0; i<n; i++)
         {
-            if(v.empty() or nums[i] > v.back())
+            if(v.empty() || nums[i]>v.back())
             {
                 v.push_back(nums[i]);
-                inc[i] = v.size();  
-            }                       
+                inc[i] = v.size();
+            }
+            
             else
             {
-                // find the index of smallest number which is >= nums[i] using Binary Search
-                int index = lower_bound(v, nums[i]);
                 
-                inc[i] = index+1;
-                v[index] = nums[i];
+                int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                v[ind] = nums[i];
+                inc[i] = ind+1;
             }
         }
-        
         
         v.clear();
-       
-        for(int i = n - 1; i >= 0; i--)
+        
+         for(int i = n-1; i>=0; i--)
         {
-            if(v.empty() or nums[i] > v.back())
+            if(v.empty() || nums[i]>v.back())
             {
                 v.push_back(nums[i]);
-                dec[i] = v.size();   
-            }                        
+                dec[i] = v.size();
+            }
+            
             else
             {
-                // find the smallest number which is >= nums[i] using Binary Search
-                int index = lower_bound(v, nums[i]);
                 
-                dec[i] = index + 1;
-                v[index] = nums[i];
+                int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                v[ind] = nums[i];
+                dec[i] = ind+1;
             }
         }
-
-        int mx = 0;
-        for(int i=1; i<nums.size()-1; i++) 
-        {
-            if(inc[i]>1 and dec[i]>1) 
-            {
-                int ans = inc[i]+dec[i]-1;
-                mx = max(mx, ans);
-            }
-        }
-        return nums.size() - mx;
         
+        int ans = 0;
+        
+        for(int i = 1; i<n-1; i++)
+        {
+           if(inc[i]>1 and dec[i]>1) 
+            {
+                ans = max(ans, inc[i] + dec[i] -1);
+            }
+            
+        }
+        
+        
+     return n - ans;
     }
 };
