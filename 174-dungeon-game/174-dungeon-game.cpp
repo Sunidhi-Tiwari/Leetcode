5b971f37814d0005fae776caeb18e436dfa46c97
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-06-19 09:27:46 +0530

    Time: 13 ms (33.76%), Space: 8.6 MB (99.16%) - LeetHub

---
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        
+        
+        int dp[m+1][n+1];
+        
+        for(int i = 0; i<=m; i++)
+            for(int j = 0; j<=n; j++)
+                dp[i][j] = INT_MAX;
+        
+        dp[m][n-1] = 1;
+        dp[m-1][n] = 1;
+            
+        for(int i = m-1; i>=0; i--)
+        {
+            for(int j = n-1; j>=0; j--)
+            {
+                
+                
+                dp[i][j]  = min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j];
+                dp[i][j] = (dp[i][j] <= 0) ? 1 : dp[i][j];
+                
+                
+            }
+        }
+      
+        return dp[0][0];
+    }
+};
