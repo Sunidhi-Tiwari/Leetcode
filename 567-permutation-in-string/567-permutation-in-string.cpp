c568aa11e99fc0710b41a42e377dfcb620341610
Sunidhi Tiwari <85020130+Sunidhi-Tiwari@users.noreply.github.com>
2022-07-03 13:00:23 +0530

    Time: 17 ms (47.74%), Space: 7.2 MB (75.44%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        
+        if( s2.length() < s1.length() ) return false;
+        
+        vector<int> arr(26);
+        vector<int> cur(26);
+        
+        for(int i = 0; i<26; i++) arr[i] = 0;
+        for(int i = 0; i<26; i++) cur[i] = 0;
+        
+        for(int i = 0; i<s1.length(); i++)
+            arr[s1[i] - 'a']++;
+        
+         for(int i = 0; i<s1.length(); i++)
+            cur[s2[i] - 'a']++;
+       
+         if(cur == arr) return true;
+        
+        for(int i = s1.length(); i< s2.length(); i++)
+        {
+            cur[s2[i - s1.length()] - 'a']--;
+            cur[s2[i] - 'a']++;
+            
+            if(cur == arr) return true;
+        }
+       
+       return false; 
+        
+        
+    }
+};
